078f1619e95faa604fdcf684f5a9b3d33bfa303a
BBarr0c0 <115066848+BBarr0c0@users.noreply.github.com>
2024-06-09 21:47:11 +0200

    Correction code

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const divImg = document.createElement('div');
+        divImg.classList.add('profile');
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
         const h2 = document.createElement( 'h2' );
+        h2.classList.add('name');
         h2.textContent = name;
-        article.appendChild(img);
+        const pCity = document.createElement('p');
+        pCity.textContent = `${city}, ${country}`;
+        pCity.classList.add('location');
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+        pTagline.classList.add('description');
+        const pPrice = document.createElement('p');
+        pPrice.textContent = `${price}€/jour`;
+        pPrice.classList.add('price');
+        article.appendChild(divImg);
+        divImg.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(pCity);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
